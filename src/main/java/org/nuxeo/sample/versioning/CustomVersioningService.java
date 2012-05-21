7724a06eef3b1233a2694dabedaddbdee5083a85
Laurent Doguin <ldoguin@nuxeo.com>
2012-05-21 14:26:07 +0200

    fix boolean parsing

--- src/main/java/org/nuxeo/sample/versioning/CustomVersioningService.java
+++ src/main/java/org/nuxeo/sample/versioning/CustomVersioningService.java
@@ -3,11 +3,15 @@ package org.nuxeo.sample.versioning;
 import java.io.Serializable;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.api.DocumentException;
 import org.nuxeo.ecm.core.model.Document;
 import org.nuxeo.ecm.core.versioning.StandardVersioningService;
 
 public class CustomVersioningService extends StandardVersioningService {
+
+    private static final Log log = LogFactory.getLog(CustomVersioningService.class);
 
 	@Override
 	public void doPostCreate(Document doc, Map<String, Serializable> options) {
@@ -16,13 +20,14 @@ public class CustomVersioningService extends StandardVersioningService {
 		}
 		try {
 			Boolean comesFromImport = (Boolean) options.get("comesFromImport");
-			if (comesFromImport != null && !"".equals(comesFromImport)) {
+			if (comesFromImport != null && comesFromImport) {
 				setVersion(doc, 12, 0);
 			} else {
 				setInitialVersion(doc);
 			}
 		} catch (DocumentException e) {
-			// ignore
+			// ignore, this should not happen
+			log.warn("Issue while setting the initial document version", e);
 		}
 	}
 }
